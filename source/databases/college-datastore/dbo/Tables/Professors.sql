02bd623b5f4b2d247e6d6456c290a631345d38aa
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-11-06 17:39:51 +0530

    Initial Version

---
+++ source/databases/college-datastore/dbo/Tables/Professors.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Professors]
+(
+	[ProfessorId] [uniqueidentifier] NOT NULL DEFAULT newid(),
+	[Name] [varchar](100) NULL,
+	[Doj] DATETIME2 NULL DEFAULT GETDATE() ,
+	[Teaches] [varchar](100) NULL,
+	[Salary] [numeric](18, 2) NULL,
+	[IsPhd] [bit] NULL, 
+    [Rating] NUMERIC(5, 2) NULL DEFAULT 2.5, 
+    CONSTRAINT [PK_Professors] PRIMARY KEY ([ProfessorId]),
+)
