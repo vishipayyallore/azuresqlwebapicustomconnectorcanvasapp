a8d200a965da4128f979d198fd8e6456647770ec
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-11-13 23:23:18 +0530

    Today's work

---
+++ source/1common/College.Core/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace College.Core.Entities
+{
+    public class Product
+    {
+    
+    }
+
+}
+
+// id= "d4abad2c-8930-4856-9ce4-a8b5afe7b617",
+//                       productId= 1,
+//                       productName= "Leaf Rake",
+//                       productCode= "GDN-0011",
+//                       releaseDate= "March 19, 2021",
+//                       description= "Leaf rake with 48-inch wooden handle.",
+//                       price= 129.95,
+//                       starRating= 3.2,
+//                       imageUrl= "assets/images/products/leaf_rake.png"
